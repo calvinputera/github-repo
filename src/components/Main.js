db1069fa7cbe9059ac68c194a7424ae36b33a2f5
Calvin Putera <104446586+calvinputera@users.noreply.github.com>
2022-07-08 15:32:53 +0700

    get data json from user profil with API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import { IoLogoGithub } from "react-icons/io";
 
 const Main = () => {
+  const [username, setUsername] = useState("");
+
+  const baseUrl = "https://api.github.com/users";
+
+  const onChageHandler = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const search = async (e) => {
+    e.preventDefault();
+
+    const profile = await fetch(`${baseUrl}/${username}`);
+    const profilJson = await profile.json();
+    console.log(profilJson);
+  };
+
   return (
     <div className="p-6 flex flex-col items-center font-noto-sans">
       <h1 className="text-3xl font-bold flex items-center">
@@ -11,11 +28,14 @@ const Main = () => {
       </h1>
       <div className="flex items-center mt-6">
         <input
+          onChange={onChageHandler}
+          value={username}
           type="text"
           placeholder="search repo..."
           className="py-2 px-3 border border-slate-500 rounded-2xl mr-2 text-sm"
         />
         <button
+          onClick={search}
           type="submit"
           className="bg-slate-900 text-white py-2 px-4 rounded-2xl text-sm"
         >
